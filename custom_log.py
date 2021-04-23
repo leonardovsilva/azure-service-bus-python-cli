1cbdca2f42544da2d797e414190dffd67978ec1d
Leonardo Viana Silva <leonardo.vsilva@stone.com.br>
2021-04-23 10:29:31 -0300

    Fix duplicated log

--- custom_log.py
+++ custom_log.py
@@ -1,36 +1,52 @@
 import logging
 import os
 import time
-from pythonjsonlogger import jsonlogger
+
+loggers = {}
+config_obj = None
 
 
 class CustomLog:
-    root_logger = logging.getLogger()
 
-    def __init__(self, config_obj):
-        try:
-            log_path = config_obj.config_export['DEFAULT']['LogPath']
-        except Exception:
-            log_path = os.path.dirname(os.path.abspath(__file__))
+    def __init__(self,  config):
+        global config_obj
+        config_obj = config
 
-        save_messages_to_file = False if config_obj.config_export['DEFAULT']['SaveMessagesToFile'] is None else bool(
-            config_obj.config_export['DEFAULT']['SaveMessagesToFile'])
+    @staticmethod
+    def get_logger(logger_name):
+        global loggers
 
-        log_formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
-        #json_log_formatter = jsonlogger.JsonFormatter()
+        if loggers.get(logger_name):
+            return loggers.get(logger_name)
+        else:
+            root_logger = logging.getLogger()
 
-        CustomLog.root_logger.setLevel(logging.INFO)
+            try:
+                log_path = config_obj.config_export['DEFAULT']['LogPath']
+            except Exception:
+                log_path = os.path.dirname(os.path.abspath(__file__))
 
-        if save_messages_to_file:
-            file_handler = logging.FileHandler("{0}/{1}.log".format(log_path, time.strftime("%Y%m%d")))
-            file_handler.setFormatter(log_formatter)
-            CustomLog.root_logger.addHandler(file_handler)
+            save_messages_to_file = False if config_obj.config_export['DEFAULT']['SaveMessagesToFile'] is None else bool(
+                config_obj.config_export['DEFAULT']['SaveMessagesToFile'])
 
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(log_formatter)
-        CustomLog.root_logger.addHandler(console_handler)
+            log_formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+
+            root_logger.setLevel(logging.INFO)
+
+            if save_messages_to_file:
+                file_handler = logging.FileHandler("{0}/{1}.log".format(log_path, time.strftime("%Y%m%d")))
+                file_handler.setFormatter(log_formatter)
+                root_logger.addHandler(file_handler)
+
+            console_handler = logging.StreamHandler()
+            console_handler.setFormatter(log_formatter)
+            root_logger.addHandler(console_handler)
+            loggers[logger_name] = root_logger
+
+            return root_logger
 
     @staticmethod
     def log_info(message):
-        CustomLog.root_logger.info(message)
+        root_logger = CustomLog.get_logger("azure-log")
+        root_logger.info(message)
 
