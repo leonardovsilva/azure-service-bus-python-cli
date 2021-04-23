5336e73bad846d781d1c975aa1bb394f5f8250dd
Leonardo Viana Silva <leonardovsilva@gmail.com>
2021-04-23 15:58:18 -0300

    get queue properties

--- queue_process.py
+++ queue_process.py
@@ -1,4 +1,5 @@
 import json
+from azure.core.exceptions import AzureError
 import custom_log
 import service_bus_base
 import service_bus_custom_encoder
@@ -13,6 +14,7 @@ class QueueProcess(service_bus_base.ServiceBusBase):
 
         print(max_message_count)
         with QueueProcess.service_bus_client:
+            QueueProcess.get_queue_properties(ctx)
             receiver = QueueProcess.service_bus_client.get_queue_receiver(queue_name=ctx.obj['QUEUE_NAME'])
             with receiver:
                 received_msgs = receiver.peek_messages(max_message_count=max_message_count)
@@ -22,3 +24,18 @@ class QueueProcess(service_bus_base.ServiceBusBase):
                 for msg in received_msgs:
                     json_str = json.dumps(msg, cls=service_bus_custom_encoder.ServiceBusCustomEncoder)
                     custom_log_obj.log_info(json_str)
+
+    @staticmethod
+    def get_queue_properties(ctx):
+        print("-- Get Queue Runtime Properties")
+        try:
+            get_queue_runtime_properties = QueueProcess.servicebus_mgmt_client.get_queue_runtime_properties(ctx.obj['QUEUE_NAME'])
+            print("Queue Name:", get_queue_runtime_properties.name)
+            print("Queue Runtime Properties:")
+            print("Updated at:", get_queue_runtime_properties.updated_at_utc)
+            print("Size in Bytes:", get_queue_runtime_properties.size_in_bytes)
+            print("Message Count:", get_queue_runtime_properties.total_message_count)
+            print("Please refer to QueueRuntimeProperties from complete available runtime properties.")
+            print("")
+        except AzureError:
+            print("Not authorized or invalid request to obtaining  queue runtime properties")
--- service_bus_base.py
+++ service_bus_base.py
@@ -1,7 +1,9 @@
 import os
-import azure.servicebus
+
+from azure.servicebus import ServiceBusClient, management
 
 
 class ServiceBusBase:
     CONNECTION_STR = os.environ['SERVICE_BUS_CONNECTION_STR']
-    service_bus_client = azure.servicebus.ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR)
+    service_bus_client = ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR)
+    servicebus_mgmt_client = management.ServiceBusAdministrationClient.from_connection_string(conn_str=CONNECTION_STR)
