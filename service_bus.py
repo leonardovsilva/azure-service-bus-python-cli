bc34f510fdf37f70043bf2aaf2887db8972ed428
Leonardo Viana Silva <leonardo.vsilva@stone.com.br>
2021-05-20 20:16:29 -0300

    Refactoring send message to a topic

--- service_bus.py
+++ service_bus.py
@@ -177,12 +177,12 @@ def message_queue(ctx, input_file):
     queue_process_obj.message(input_file)
 
 
-@main.command('message_sub')
+@main.command('message_topic')
 @click.pass_context
 @click.option('--input_file', '-i', type=click.File('rb'), required=True, help="Directory to get message file")
-def message_subscription(ctx, input_file):
+def message_topic(ctx, input_file):
     """
-            :   Send messages to a service bus queue
+            :   Send messages to a service bus topic
     """
 
     topic_process_obj = TopicProcess(ctx)
